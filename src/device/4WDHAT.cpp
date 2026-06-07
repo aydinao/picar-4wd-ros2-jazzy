84b44e062353bf07d0869642ef500aa24101dcf0
AydinAO <92227214+aydinao@users.noreply.github.com>
2026-06-07 21:21:55 +0100

    complete set frequency

--- src/device/4WDHAT.cpp
+++ src/device/4WDHAT.cpp
@@ -1,7 +1,7 @@
 #include "4WDHAT.hpp"
 #include <cmath>
-#include <ranges>
 #include <vector>
+#include <algorithm>
 
 namespace PiCar_4WD{
     PiCar4WDHAT::PiCar4WDHAT(int bus, uint_fast8_t address, uint8_t channel) : Device(bus, address), channel_(channel) { }
@@ -26,24 +26,31 @@ namespace PiCar_4WD{
             frequency = 50;
         }
     
-        int start = sqrt(CLOCK / frequency);
+        int start = std::sqrt(CLOCK / frequency);
         start = start - 5; 
         // prevent negative value
         if (start <= 0) 
         {
             start = 1;
         }
-        std::vector< int > arr;
+        std::vector<std::pair< int, int >> candidates;
         std::vector< int > accuracy; 
 
         for (int prescaler = start; prescaler < start + 10; prescaler++)
         {
             int val = CLOCK / frequency / prescaler;
-            arr.push_back(val);
-            accuracy.push_back(abs(frequency - CLOCK / prescaler / val));
+            candidates.push_back({prescaler, val});
+            accuracy.push_back(std::abs(frequency - CLOCK / prescaler / val));
         }
         
-    
+        std::vector<int>::iterator result = std::min_element(std::begin(accuracy), std::end(accuracy));
+        auto index = std::distance(accuracy.begin(), result);
+        int prescaler = candidates[index].first;
+        int period = candidates[index].second;
+
+        set_prescaler(static_cast<uint8_t>(prescaler));
+        set_period(static_cast<uint16_t>(period));
+
     }
 
 }
