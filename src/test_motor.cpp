737fec5b0aceb8dd9f16b838516f2bdb729a3dab
AydinAO <92227214+aydinao@users.noreply.github.com>
2026-06-11 19:51:09 +0100

    add test_motor.cpp

---
+++ src/test_motor.cpp
@@ -0,0 +1,29 @@
+#include "motor.hpp"
+#include <chrono>
+#include <thread>
+
+int main() {
+    uint8_t left_front_gpio = 23;
+    uint8_t right_front_gpio = 24;
+    uint8_t left_rear_gpio = 13;
+    uint8_t right_rear_gpio = 20;
+    
+    uint8_t left_front_pwm = 13;
+    uint8_t right_front_pwm = 12;
+    uint8_t left_rear_pwm = 8;
+    uint8_t right_rear_pwm = 9;
+
+    PiCar_4WD::PiCar4WDHAT pwm_left_front_obj(1, 0x14, left_front_pwm); 
+    PiCar_4WD::PiCar4WDHAT pwm_right_front_obj(1, 0x14, right_front_pwm); 
+    PiCar_4WD::PiCar4WDHAT pwm_left_rear_obj(1, 0x14, left_rear_pwm); 
+    PiCar_4WD::PiCar4WDHAT pwm_right_rear_obj(1, 0x14, right_rear_pwm); 
+
+    PiCar_4WD::Motor left_front(pwm_left_front_obj, left_front_gpio);
+    PiCar_4WD::Motor right_front(pwm_right_front_obj, right_front_gpio);
+    PiCar_4WD::Motor left_rear(pwm_left_rear_obj, left_rear_gpio);
+    PiCar_4WD::Motor right_rear(pwm_right_rear_obj, right_rear_gpio);
+
+    left_front.set_power(60);
+
+
+}
